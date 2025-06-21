cae5a0804d5c98eb229d59d0625353e17df94659
gariff15 <chocoumaii16@gmail.com>
2025-06-21 22:42:47 +0800

    Update 13-06-2025.py

--- 13-06-2025.py
+++ 13-06-2025.py
@@ -297,7 +297,7 @@ def show_header():
         color: white;
         text-align: center;
         animation: pulseGlow 3s infinite ease-in-out;
-        margin-bottom: 0.5rem;
+        margin-bottom: -1rem;
     }
     </style>
 
